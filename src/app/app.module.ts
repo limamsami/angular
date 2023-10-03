dba914ea5ef8b2152d89aa715bc465aafb168537
Sami-Limam-TSI <sami.limam@tsi.com.tn>
2023-10-03 11:12:41 +0100

    customFilter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,32 @@
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule }
-	from '@angular/platform-browser/animations';
-import { AppComponent } from './app.component';
-import { TableModule } from 'primeng/table';
-import { CalendarModule } from 'primeng/calendar';
-import { SliderModule } from 'primeng/slider';
-import { DialogModule } from 'primeng/dialog';
-import { MultiSelectModule } from 'primeng/multiselect';
-import { ContextMenuModule } from 'primeng/contextmenu';
-import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
-import { ProgressBarModule } from 'primeng/progressbar';
-import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent }   from './app.component';
+import {CarService} from './carservice';
+
+import {ToastModule} from 'primeng/toast';
+import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
+import {AutoCompleteModule} from 'primeng/autocomplete';
+import {InputTextModule} from 'primeng/inputtext';
 
 @NgModule({
-	imports: [
-		BrowserModule,
-		BrowserAnimationsModule,
-		TableModule,
-		CalendarModule,
-		SliderModule,
-		DialogModule,
-		MultiSelectModule,
-		ContextMenuModule,
-		DropdownModule,
-		ButtonModule,
-		InputTextModule,
-		ProgressBarModule,
-		HttpClientModule,
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+		AutoCompleteModule,
 		FormsModule,
-	],
-	declarations: [AppComponent],
-	bootstrap: [AppComponent],
+		TableModule,
+		InputTextModule,
+    HttpClientModule
+  ],
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ],
+  providers: [CarService]
 })
 
-export class AppModule {}
+export class AppModule { }
